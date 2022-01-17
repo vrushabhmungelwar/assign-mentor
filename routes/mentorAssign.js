91bb39beefe467eaa64df350673317dba5cb02ab
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2022-01-17 22:33:43 +0530

    error

--- routes/mentorAssign.js
+++ routes/mentorAssign.js
@@ -28,17 +28,17 @@ router.post("/newMentor", async (req, res) => {
 router.post("/modifyMentor", async (req, res) => {
   try {
     let stud = await student.findById(req.body.studentId);
-    // let oldmentid = stud.mentorAssigned;
+    let oldmentid = stud.mentorAssigned;
 
     stud.mentorAssigned = req.body.newMentorId;
     stud.save();
-//
-    // let oldment = await mentor.findById(oldmentid);
-    // var new_list = oldment.studentsAssigned;
-    // new_list = new_list.filter((e) => e !== req.body.studentId);
+    //
+    let oldment = await mentor.findById(oldmentid);
+    var new_list = oldment.studentsAssigned;
+    new_list = new_list.filter((e) => e !== req.body.studentId);
 
-    // oldment.studentsAssigned = new_list;
-//
+    oldment.studentsAssigned = new_list;
+    //
 
     // if (oldment.studentsAssigned.length < 0) {
     //   // console.log("oldment");
@@ -52,27 +52,15 @@ router.post("/modifyMentor", async (req, res) => {
     //   oldment.studentsAssigned = newAssigned;
     // }
 
-    // oldment.save();    ------
+    oldment.save();   
 
     let newment = await mentor.findById(req.body.newMentorId);
-    // var new_list1 = newment.studentsAssigned;
-    // new_list1 = new_list1.push(req.body.studentId);
-    // newment.studentsAssigned = new_list1;
 
     newment.studentsAssigned = [
       ...newment.studentsAssigned,
       req.body.studentId,
     ];
-    // if (newment.studentsAssigned.length < 0) {
-    //   return;
-    // } else {
-    //   if (!newment.studentsAssigned.includes(req.body.studentId)) {
-    //     newment.studentsAssigned = [
-    //       ...newment.studentsAssigned,
-    //       req.body.studentId,
-    //     ];
-    //   }
-    // }
+
     newment.save();
 
     res.send("Assigned a student to a new mentor");
